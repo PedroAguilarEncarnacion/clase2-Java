88725d68a35b2f33bf0785531037eca5ff9cbd4a
Pedro Aguilar <pedro_stalyn@hotmail.com>
2025-04-07 16:56:59 -0500

    Avtulizacion

--- src/main/java/ec/edu/espoch/clase2/Clase2.java
+++ src/main/java/ec/edu/espoch/clase2/Clase2.java
@@ -1,17 +1,54 @@
 package ec.edu.espoch.clase2;
 
+import java.util.Scanner;
+
 public class Clase2 {
 
     public static void main(String[] args) {
         
-        int multiplicador=1;
-        int i=0;
-        while (multiplicador<=20) { 
-            i +=multiplicador;
-           
-            System.out.println(multiplicador+" X "+ i+" ="+(i*i));
-            i++;
-            
+        Scanner e = new Scanner(System.in);
+
+        System.out.println("Escoja una opcion");
+        System.out.println("1.Suma");
+        System.out.println("2.resta");
+        System.out.println("3.Multiplicacion");
+        int opcion = e.nextInt();
+        int a;
+        int b;
+        int suma;
+        int multiplicacion;
+        switch (opcion) {
+            case 1:
+
+                System.out.println("Ingrese el primer numero");
+                a= e.nextInt();
+                
+                System.out.println("Ingrese el segundo numero");
+                b= e.nextInt();
+                suma= a + b;
+                System.out.println("La suma es: " +suma);
+            case 2:
+                System.out.println("Ingrese el primer numero");
+                a= e.nextInt();
+                
+                System.out.println("Ingrese el segundo numero");
+                b= e.nextInt();
+                int resta= a - b;
+                System.out.println("El resultado es "+resta);
+                break;
+            case 3:
+                System.out.println("Ingrese el primer numero");
+                a= e.nextInt();
+                
+                System.out.println("Ingrese el segundo numero");
+                b= e.nextInt();
+                multiplicacion= a * b;
+                System.out.println("El resultado es "+multiplicacion);
+                break;
+            default:
+                System.out.println("Opcion incorrecta");;
         }
-    }
+
+        
+            }
 }
